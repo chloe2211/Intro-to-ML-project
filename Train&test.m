d4a5071adcfea0562b812b34149c821dddc31c89
Claudia Shi <j.c.shi@lse.ac.uk>
2017-11-01 15:22:01 -0500

    Update Train&test.m

--- Train&test.m
+++ Train&test.m
@@ -1,63 +1,39 @@
 %% updating right now, please come back later
-# cost function 
-function checkCostFunction(lambda)
-%CHECKCOSTFUNCTION Creates a collaborative filering problem 
-% Set lambda
-if ~exist('lambda', 'var') || isempty(lambda)
-    lambda = 0;
-end
-%% Create small problem
-X_t = rand(4, 3);
-Theta_t = rand(5, 3);
+%%% Training
+dataset=read_dataset('../ml-100k/u3.base',0);
+save('dataset','dataset');
 
-% Zap out most entries
-Y = X_t * Theta_t';
-Y(rand(size(Y)) > 0.5) = 0;
-R = zeros(size(Y));
-R(Y ~= 0) = 1;
+train_ratings=zeros(943,1682);
 
-%% Run Gradient Checking
-X = randn(size(X_t));
-Theta = randn(size(Theta_t));
-num_users = size(Y, 2);
-num_movies = size(Y, 1);
-num_features = size(Theta_t, 2);
-
-numgrad = computeNumericalGradient( ...
-                @(t) cofiCostFunc(t, Y, R, num_users, num_movies, ...
-                                num_features, lambda), [X(:); Theta(:)]);
-
-[cost, grad] = cofiCostFunc([X(:); Theta(:)],  Y, R, num_users, ...
-                          num_movies, num_features, lambda);
-
-
-diff = norm(numgrad-grad)/norm(numgrad+grad);
-
-end
-
-
-%% select Threshold
-function [bestEpsilon bestF1] = selectThreshold(yval, pval)
- bestEpsilon = 0;
-  bestF1 = 0;
-  F1 = 0;
-
-  stepsize = (max(pval) - min(pval)) / 1000;
-  for epsilon = min(pval):stepsize:max(pval)
-   cvPredictions = (pval < epsilon);
-    tp = sum((cvPredictions == 1) & (yval == 1));
-    fp = sum((cvPredictions == 1) & (yval == 0));
-    fn = sum((cvPredictions == 0) & (yval == 1));
-
-    prec = tp/(tp + fp);
-    rec = tp/(tp + fn);
-
-    F1 = 2*prec*rec/(prec + rec);
-
-    if F1 > bestF1
-      bestF1 = F1;
-      bestEpsilon = epsilon;
+for i=1:1682
+    for j=1:943
+        ind=find((dataset(1,:)==j) & (dataset(2,:)==i));
+        if isempty(ind)
+            train_ratings(j,i)=0;
+        else
+            train_ratings(j,i)=dataset(3,ind);
+        end
     end
-  end
-
+    if (mod(i,50)==0)
+        disp(i);
+    end
 end
+save('train_ratings','train_ratings');
+%%%Testing
+testset=read_dataset('../ml-100k/u3.test',0);
+save('testset','testset');
+test_ratings=zeros(943,1682);
+for i=1:1682
+    for j=1:943
+        ind=find((testset(1,:)==j) & (testset(2,:)==i));
+        if isempty(ind)
+            test_ratings(j,i)=0;
+        else
+            test_ratings(j,i)=testset(3,ind);
+        end
+    end
+    if (mod(i,50)==0)
+        disp(i);
+    end
+end
+save('test_ratings','test_ratings');
