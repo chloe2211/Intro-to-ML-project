64852960789fcc47987a0e3af1478b155805bd7b
Claudia Shi <j.c.shi@lse.ac.uk>
2017-10-31 21:01:24 -0500

    Create Train&test.m

---
+++ Train&test.m
@@ -0,0 +1,19 @@
+# Intro-to-ML-project
+
+input_layer_size  = 400;  % 20x20 Input Images of Digits
+hidden_layer_size = 25;   % 25 hidden units
+num_labels = 10;          % 10 labels, from 1 to 10
+fprintf('Loading and Visualizing Data ...\n')
+
+load('ex3data1.mat');
+m = size(X, 1);
+
+% Randomly select 100 data points to display
+sel = randperm(size(X, 1));
+sel = sel(1:100);
+
+displayData(X(sel, :));
+
+fprintf('Program paused. Press enter to continue.\n');
+pause;
+
