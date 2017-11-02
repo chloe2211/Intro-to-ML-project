61b3c59a5cdc88309fd597e316820fc88d570d93
Claudia Shi <j.c.shi@lse.ac.uk>
2017-11-02 01:31:53 -0500

    Update Train&test.m

--- Train&test.m
+++ Train&test.m
@@ -1,5 +1,5 @@
 %% updating right now, please come back later
 %%% we use LibSVM in this case
-[trainlabels, trainfeatures] = libsvmread('u.data');
+[trainlabels, trainfeatures,trainfeatures,trainfeatures] = libsvmread('u.data');
 %% setting cost to c=1
-model = svmtrain(trainlabels, trainfeatures, '-s 0 -t 0 -c 1');
+model = svmtrain(trainlabels, trainfeatures,trainfeatures,trainfeatures, '-s 0 -t 0 -c 1');
