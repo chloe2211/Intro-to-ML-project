7a7899b6dc1fcef4f3e43c3eda2b3bef790281d5
Claudia Shi <j.c.shi@lse.ac.uk>
2017-11-03 18:55:11 -0500

    Update Train&test.m

--- Train&test.m
+++ Train&test.m
@@ -3,3 +3,10 @@
 [trainlabels, trainfeatures,trainfeatures,trainfeatures] = libsvmread('u.data');
 %% setting cost to c=1
 model = svmtrain(trainlabels, trainfeatures,trainfeatures,trainfeatures, '-s 0 -t 0 -c 1');
+w = model.SVs' * model.sv_coef;
+b = -model.rho;
+if (model.Label(1) == -1)
+    w = -w; b = -b;
+end
+
+plot(w,b)
