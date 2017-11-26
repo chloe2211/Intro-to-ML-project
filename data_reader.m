a15789b862ef6df7faa7466241b4c08cdb82b130
Claudia Shi <j.c.shi@lse.ac.uk>
2017-11-26 00:36:38 -0600

    Update data_reader.m

--- data_reader.m
+++ data_reader.m
@@ -2,3 +2,11 @@ A = importdata('u.data');
 user_id = A(:, 1);
 movie_id = A(:, 2);
 rating = A(:, 3);
+% initialize the result matrix and weight matrix
+Res = zeros(943, 1682);
+weight = zeros(943, 1682);
+for i=1:100000
+     R(user_id(i), movie_id(i)) = rating(i);
+     w(user_id(i), movie_id(i)) = 1;
+ end
+ % we select three K values
