187b9f4d699d441d23a662adb1e270b300c96186
Claudia Shi <j.c.shi@lse.ac.uk>
2017-11-27 18:52:39 -0600

    Update Train&test.m

--- Train&test.m
+++ Train&test.m
@@ -8,10 +8,10 @@ for i=1:100000
 
  k = [10, 50, 100];
 
- % Factorize using clrule
- [U11,V11,Iteration,tElapsed,finalRes11]=clrule(R, k(1), w);
- [U12,V12,Iteration,tElapsed,finalRes12]=clrule(R, k(2), w);
- [U13,V13,Iteration,tElapsed,finalRes13]=clrule(R, k(3), w);
+ % Factorize using mycfrule
+ [U11,V11,Iteration,tElapsed,finalRes11]=mycfrule(R, k(1), w);
+ [U12,V12,Iteration,tElapsed,finalRes12]=mycfrule(R, k(2), w);
+ [U13,V13,Iteration,tElapsed,finalRes13]=mycfrule(R, k(3), w);
  
  % lse is least error square
  lse(1,1) = finalRes11; 
@@ -27,10 +27,10 @@ for i=1:100000
 % % Three different values of k
  k = [10, 50, 100];
 % 
-% % Factorize using clrule
- [U21,V21,Iteration,tElapsed,finalRes21]=clrule(R2,k(1), w2);
- [U22,V22,Iteration,tElapsed,finalRes22]=clrule(R2,k(2), w2);
- [U23,V23,Iteration,tElapsed,finalRes23]=clrule(R2,k(3), w2);
+% % Factorize using mycfrule
+ [U21,V21,Iteration,tElapsed,finalRes21]=mycfrule(R2,k(1), w2);
+ [U22,V22,Iteration,tElapsed,finalRes22]=mycfrule(R2,k(2), w2);
+ [U23,V23,Iteration,tElapsed,finalRes23]=mycfrule(R2,k(3), w2);
  
  lse2(1,1) = finalRes21; 
  lse2(2,1) = finalRes22; 
